59b0017f2731c0d7eecd23a3cd8479a6dc977a33
baraasowwan1 <105966196+baraasowwan1@users.noreply.github.com>
2026-02-05 16:59:33 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
+import { createRoot } from "react-dom/client";
+import App from "./app/App";
+import "./styles/index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+// استدعاء إعدادات اللغات
+import "./i18n";
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(<App />);
