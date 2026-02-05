dc7cf18478b190daec5390a9c4829a3ecaa4b6d1
baraasowwan1 <105966196+baraasowwan1@users.noreply.github.com>
2026-02-05 17:02:57 +0300

    Update index.ts

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,15 +3,22 @@ import { initReactI18next } from "react-i18next";
 import ar from "./ar.json";
 import en from "./en.json";
 
-const userLang = navigator.language.startsWith("ar") ? "ar" : "en";
+// تحديد اللغة:
+// 1) لغة محفوظة
+// 2) لغة الجهاز
+// 3) العربية افتراضيًا
+const savedLang = localStorage.getItem("lang");
+const browserLang = navigator.language.startsWith("ar") ? "ar" : "en";
+
+const defaultLang = savedLang || browserLang || "ar";
 
 i18n.use(initReactI18next).init({
   resources: {
     ar: { translation: ar },
     en: { translation: en }
   },
-  lng: userLang,        // حسب لغة الجهاز
-  fallbackLng: "ar",    // العربية افتراضيًا
+  lng: defaultLang,
+  fallbackLng: "ar",
   interpolation: {
     escapeValue: false
   }
